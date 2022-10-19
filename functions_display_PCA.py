cc407d94a5b54b5338fc2beceaab62ea7f6699b7
kevin <eleorekevin@live.fr>
2022-10-19 15:52:55 +0200

    Completer nutri score

---
+++ functions_display_PCA.py
@@ -0,0 +1,105 @@
+def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
+    '''Display a scatter plot on a factorial plane, one for each factorial plane'''
+
+    # For each factorial plane
+    for d1,d2 in axis_ranks:
+        if d2 < n_comp:
+ 
+            # Initialise the matplotlib figure      
+            fig = plt.figure(figsize=(7,6))
+        
+            # Display the points
+            if illustrative_var is None:
+                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
+            else:
+                illustrative_var = np.array(illustrative_var)
+                for value in np.unique(illustrative_var):
+                    selected = np.where(illustrative_var == value)
+                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
+                plt.legend()
+
+            # Display the labels on the points
+            if labels is not None:
+                for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
+                    plt.text(x, y, labels[i],
+                              fontsize='14', ha='center',va='center') 
+                
+            # Define the limits of the chart
+            boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
+            plt.xlim([-boundary,boundary])
+            plt.ylim([-boundary,boundary])
+        
+            # Display grid lines
+            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
+
+            # Label the axes, with the percentage of variance explained
+            plt.xlabel('PC{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('PC{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
+
+            plt.title("Projection of points (on PC{} and PC{})".format(d1+1, d2+1))
+            #plt.show(block=False)
+            
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import numpy as np
+import pandas as pd
+from scipy.cluster.hierarchy import dendrogram
+from pandas.plotting import parallel_coordinates
+import seaborn as sns
+
+palette = sns.color_palette("bright", 10)
+
+def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
+    """Display correlation circles, one for each factorial plane"""
+
+    # For each factorial plane
+    for d1, d2 in axis_ranks: 
+        if d2 < n_comp:
+
+            # Initialise the matplotlib figure
+            fig, ax = plt.subplots(figsize=(10,10))
+
+            # Determine the limits of the chart
+            if lims is not None :
+                xmin, xmax, ymin, ymax = lims
+            elif pcs.shape[1] < 30 :
+                xmin, xmax, ymin, ymax = -1, 1, -1, 1
+            else :
+                xmin, xmax, ymin, ymax = min(pcs[d1,:]), max(pcs[d1,:]), min(pcs[d2,:]), max(pcs[d2,:])
+
+            # Add arrows
+            # If there are more than 30 arrows, we do not display the triangle at the end
+            if pcs.shape[1] < 30 :
+                plt.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
+                   pcs[d1,:], pcs[d2,:], 
+                   angles='xy', scale_units='xy', scale=1, color="grey")
+                # (see the doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
+            else:
+                lines = [[[0,0],[x,y]] for x,y in pcs[[d1,d2]].T]
+                ax.add_collection(LineCollection(lines, axes=ax, alpha=.1, color='black'))
+            
+            # Display variable names
+            if labels is not None:  
+                for i,(x, y) in enumerate(pcs[[d1,d2]].T):
+                    if x >= xmin and x <= xmax and y >= ymin and y <= ymax :
+                        plt.text(x, y, labels[i], fontsize='14', ha='center', va='center', rotation=label_rotation, color="blue", alpha=0.5)
+            
+            # Display circle
+            circle = plt.Circle((0,0), 1, facecolor='none', edgecolor='b')
+            plt.gca().add_artist(circle)
+
+            # Define the limits of the chart
+            plt.xlim(xmin, xmax)
+            plt.ylim(ymin, ymax)
+        
+            # Display grid lines
+            plt.plot([-1, 1], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-1, 1], color='grey', ls='--')
+
+            # Label the axes, with the percentage of variance explained
+            plt.xlabel('PC{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('PC{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
+
+            plt.title("Correlation Circle (PC{} and PC{})".format(d1+1, d2+1))
+            plt.show(block=False)
